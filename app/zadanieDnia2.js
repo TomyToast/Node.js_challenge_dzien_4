48ef8bda3af66dc8a330aa167059bc43ad339e62
Tomek Kulczyński <kulczynski.t@gmail.com>
2019-03-11 21:54:27 +0100

    finished task zadanieDnia2

--- app/zadanieDnia2.js
+++ app/zadanieDnia2.js
@@ -1,1 +1,18 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req,res) => {
+    const acceptLanguage = req.headers['accept-language'];
+    const userAgent = req.headers['user-agent'];
+    const acceptContent = req.headers['accept'];
+
+    console.log('preferowane jezyki: ', acceptLanguage);
+    // console.log('info: ', userAgent);
+    // console.log('preferowane tresci: ', acceptContent);
+
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(userAgent)
+});
+
+srv.listen(3000, () => {
+    console.log('serwer uruchomiony na porcie 3000');
+})//Twój kod
